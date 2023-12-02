a5b0b370e5df28f5f0722fae6a80c11c67659784
radek-nowak <radeknowak1999@gmail.com>
2023-12-02 20:05:33 +0100

    sample get endpoint

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type team struct {
+	ID    int32  `json:"id"`
+	Namne string `json:"name"`
+	Stars int    `json:"stars"`
+}
+
+var teams = []team{
+	{ID: 1, Namne: "Liverpool", Stars: 5},
+	{ID: 2, Namne: "Manchester City", Stars: 5},
+}
+
+func getTeams(ctx *gin.Context) {
+	ctx.IndentedJSON(http.StatusOK, teams)
+}
 
 func main() {
-	fmt.Println("Hello world")
+	router := gin.Default()
+	router.GET("/teams", getTeams)
+	router.Run("localhost:9090")
 }
