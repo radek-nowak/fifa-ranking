5d60d81d6f95640b094e84e6c547c3a19d7e7730
radek-nowak <radeknowak1999@gmail.com>
2023-12-03 18:43:54 +0100

    import controller

--- cmd/main.go
+++ cmd/main.go
@@ -3,16 +3,16 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 
-	"ranking/cmd/controllers"
+	controller "github.com/radek-nowak/fifa-ranking/internal/team/controller"
 )
 
 func main() {
 	router := gin.Default()
 
-	router.GET("/teams", controllers.GetTeams)
-	router.GET("/teams/:name", controllers.GetTeamByName)
+	router.GET("/teams", controller.GetTeams)
+	router.GET("/teams/:name", controller.GetTeamByName)
 
-	router.POST("/teams", controllers.AddTeam)
+	router.POST("/teams", controller.AddTeam)
 
 	router.Run("localhost:9090")
 }
